572a356bb18f7ac63556143d874abc57065f37ee
nikio <ked_nik@mail.ru>
2021-05-13 16:29:52 +0600

    Второе сохранение

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import pandas as pd
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    df = pd.DataFrame({'one':[1,2,3]})
     return 'Hello World!'
 
 
